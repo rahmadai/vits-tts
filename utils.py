a6681078fa64893c96294b213598b85a1de6cb15
rahmadai <rahmadkurniawan.ai@gmail.com>
2021-11-05 13:58:48 +0700

    print load filepath and text

--- utils.py
+++ utils.py
@@ -138,6 +138,7 @@ def load_wav_to_torch(full_path):
 def load_filepaths_and_text(filename, split="|"):
   with open(filename, encoding='utf-8') as f:
     filepaths_and_text = [line.strip().split(split) for line in f]
+    print(filepaths_and_text)
   return filepaths_and_text
 
 
