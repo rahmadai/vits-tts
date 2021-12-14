54518195374e2d76159c8962e0bc5cd65217ad35
rahmadai <rahmadkurniawan.ai@gmail.com>
2021-12-14 23:16:08 +0700

    configure

--- README.md
+++ README.md
@@ -1,36 +1,25 @@
-# VIT TTS
-
-
+# End-to-End VITS TTS
 
 ## Pre-requisites
 0. Python >= 3.6
 0. Clone this repository
 0. Install python requirements. Please refer [requirements.txt](requirements.txt)
-    1. You may need to install espeak first: `apt-get install espeak`
-0. Download datasets
-    1. Download and extract the LJ Speech dataset, then rename or create a link to the dataset folder: `ln -s /path/to/LJSpeech-1.1/wavs DUMMY1`
-    1. For mult-speaker setting, download and extract the VCTK dataset, and downsample wav files to 22050 Hz. Then rename or create a link to the dataset folder: `ln -s /path/to/VCTK-Corpus/downsampled_wavs DUMMY2`
-0. Build Monotonic Alignment Search and run preprocessing if you use your own datasets.
+    1. Install espeak first: `apt-get install espeak`
+0. Build Monotonic Alignment Search and run preprocessing.
 ```sh
 # Cython-version Monotonoic Alignment Search
 cd monotonic_align
 python setup.py build_ext --inplace
 
-# Preprocessing (g2p) for your own datasets. Preprocessed phonemes for LJ Speech and VCTK have been already provided.
-# python preprocess.py --text_index 1 --filelists filelists/ljs_audio_text_train_filelist.txt filelists/ljs_audio_text_val_filelist.txt filelists/ljs_audio_text_test_filelist.txt 
-# python preprocess.py --text_index 2 --filelists filelists/vctk_audio_sid_text_train_filelist.txt filelists/vctk_audio_sid_text_val_filelist.txt filelists/vctk_audio_sid_text_test_filelist.txt
+# Preprocessing (g2p) for your own datasets. Preprocessed phonemes.
+# python preprocess.py --text_index 2 --filelists filelists/backup/wdy_audio_text_train_filelist_5.txt filelists/backup/wdy_audio_text_train_filelist_4.txt filelists/backup/wdy_audio_text_train_filelist_3.txt filelists/backup/wdy_audio_text_train_filelist_2.txt filelists/backup/wdy_audio_text_train_filelist_1.txt
+
 ```
 
 
-## Training Exmaple
+## Training
 ```sh
-# LJ Speech
-python train.py -c configs/ljs_base.json -m ljs_base
 
-# VCTK
-python train_ms.py -c configs/vctk_base.json -m vctk_base
+# Widya Multispeaker
+python train_ms.py -c configs/wdy_multispeaker_base.json -m wdy_multispeaker
 ```
-
-
-## Inference Example
-See [inference.ipynb](inference.ipynb)
--- inferencer.py
+++ inferencer.py
@@ -8,6 +8,7 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 from torch.utils.data import DataLoader
+import time
 
 import commons
 import utils
@@ -26,21 +27,45 @@ def get_text(text, hps):
     text_norm = torch.LongTensor(text_norm)
     return text_norm
 
-hps = utils.get_hparams_from_file("/home/server/rahmad/Work/WIN/AI/Speech/vits/model/wdy_indo_new_dataset/config.json")
+hps = utils.get_hparams_from_file("/home/server/rahmad/Work/WIN/AI/Speech/vits/model/wdy_multispeaker/config.json")
 
 net_g = SynthesizerTrn(
     len(symbols),
     hps.data.filter_length // 2 + 1,
     hps.train.segment_size // hps.data.hop_length,
+    n_speakers=hps.data.n_speakers,
     **hps.model).cuda()
 _ = net_g.eval()
 
-_ = utils.load_checkpoint("/home/server/rahmad/Work/WIN/AI/Speech/vits/model/wdy_indo_new_dataset/G_19000.pth", net_g, None)
+_ = utils.load_checkpoint("/home/server/rahmad/Work/WIN/AI/Speech/vits/model/wdy_multispeaker/G_124000.pth", net_g, None)
 
-stn_tst = get_text("Indonesia merupakan negara kepulauan terbesar di dunia yang memiliki potensi untuk menjadi Poros Maritim Dunia. Poros Maritim Dunia bertujuan menjadikan Indonesia sebagai negara maritim yang besar, kuat, dan makmur melalui pengembalian identitas Indonesia sebagai bangsa maritim, pengamanan kepentingan dan keamanan maritim, memberdayakan potensi maritim untuk mewujudkan pemerataan ekonomi Indonesia.", hps)
-with torch.no_grad():
-    x_tst = stn_tst.cuda().unsqueeze(0)
-    x_tst_lengths = torch.LongTensor([stn_tst.size(0)]).cuda()
-    audio = net_g.infer(x_tst, x_tst_lengths, noise_scale=.667, noise_scale_w=0.8, length_scale=1)[0][0,0].data.cpu().float().numpy()
 
-sf.write("out.wav", audio, 22050, "PCM_16")
+
+
+
+while(True):
+    print("Masukkan Teks :")
+    text_input = input("")  # Python 3
+    print("Masukkan speaker :")
+    speaker_id = input("")
+    if(speaker_id == "ayas"):
+        speaker_id = 0
+    elif(speaker_id == "jean"):
+        speaker_id = 1
+    elif(speaker_id == "moza"):
+        speaker_id = 2
+    elif(speaker_id == "davar"):
+        speaker_id = 3
+    start_time = time.time()
+    # stn_tst = get_text("Di sebuah hutan, tinggallah keluarga Bebek di tengah hutan yang terdiri dari Ayah dan empat anak Bebek. Ayah Bebek mempunyai anak bungsu yang lucu dan baik hati, ketiga kakaknya sangat menyanyanginya. Karena sifatnya itulah, ia sangat disayang oleh seluruh binatang hutan.", hps)
+    stn_tst = get_text(str(text_input), hps)
+    with torch.no_grad():
+        x_tst = stn_tst.cuda().unsqueeze(0)
+        x_tst_lengths = torch.LongTensor([stn_tst.size(0)]).cuda()
+        sid = torch.LongTensor([int(speaker_id)]).cuda()
+        audio = net_g.infer(x_tst, x_tst_lengths, sid=sid, noise_scale=.667, noise_scale_w=0.8, length_scale=1)[0][0,0].data.cpu().float().numpy()
+    end_time = time.time()
+
+    process_time_tts=end_time-start_time
+    print(process_time_tts) 
+    sf.write("out.wav", audio, 22050, "PCM_16")
